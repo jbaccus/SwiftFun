3cafd94f56ee2668f1c47b51069bdea4d07c117a
jbaccus <jbaccus@gmail.com>
2017-12-20 21:11:37 -0800

    the first commit

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+var buttonClickCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        buttonClickCount = buttonClickCount + 1
+        print(buttonClickCount)
+        if buttonClickCount >= 5 {
+            myLabel.text = "You clicked the button mother fucker!"
+            view.backgroundColor = UIColor.green
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.red
+        myLabel.text = "Hi Muther Fucker!!"
     }
 
     override func didReceiveMemoryWarning() {
